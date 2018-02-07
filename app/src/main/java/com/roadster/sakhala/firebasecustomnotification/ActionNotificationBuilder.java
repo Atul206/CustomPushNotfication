b3f75f24b9d3dd1a0c017ee4a359b21c34d9c018
Atul Sakhala <atul.sakhala@rivigo.com>
2018-02-07 15:10:55 +0530

    Changes with custom style

--- app/src/main/java/com/roadster/sakhala/firebasecustomnotification/ActionNotificationBuilder.java
+++ app/src/main/java/com/roadster/sakhala/firebasecustomnotification/ActionNotificationBuilder.java
@@ -8,7 +8,10 @@ import android.graphics.Bitmap;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by atulsakhala on 31/01/18.
@@ -18,6 +21,7 @@ public class ActionNotificationBuilder {
 
     public static final String BUTTON_ACTION = "button_action";
     public static final String BUTTON_ICON = "button_icon";
+    public static final String UPDATE_STATUS_ID = "update_status_id";
     private static final String TAG = ActionNotificationBuilder.class.getName();
     private NotificationCompat.Builder mBuilder;
     private NotificationManager notificationManager;
@@ -26,59 +30,12 @@ public class ActionNotificationBuilder {
     private Bitmap mNotificationBigIconResounce;
     private String title;
     private String subTitle;
+    private String backGroundImageUrl;
 
     public ActionNotificationBuilder(Context activity) {
         this.context = activity;
         mBuilder = new NotificationCompat.Builder(activity);
         notificationManager = (NotificationManager) activity.getSystemService(activity.NOTIFICATION_SERVICE);
-    }
-
-
-    public void setNotificationType(NotificationType type, Intent intent) {
-        switch (type) {
-            case EXPANDABLE_NOTIFICATION:
-
-
-            case DOUBLE_ACTION_NOTIFICATION:
-                PendingIntent doubleActionIntent = PendingIntent.getActivity(
-                        context,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-
-                mBuilder
-                        .setSmallIcon(android.R.drawable.ic_dialog_info)
-                        .setContentTitle("Something important happened")
-                        .setContentText("See the details")
-                        .setAutoCancel(true)
-                        .setContentIntent(doubleActionIntent)
-                        .addAction(android.R.drawable.ic_menu_compass, "Details", doubleActionIntent)
-                        .addAction(android.R.drawable.ic_menu_directions, "Show Map", doubleActionIntent);
-
-                notificationManager.notify(852, mBuilder.build());
-                break;
-            case SINGLE_ACTION_NOTIFICATION:
-                PendingIntent singleIntent = PendingIntent.getActivity(
-                        context,
-                        0,
-                        intent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-
-                mBuilder
-                        .setSmallIcon(android.R.drawable.ic_dialog_info)
-                        .setContentTitle("Something important happened")
-                        .setContentText("See the details")
-                        .setAutoCancel(true)
-                        .setContentIntent(singleIntent)
-                        .addAction(android.R.drawable.ic_menu_compass, "Details", singleIntent)
-                        .addAction(android.R.drawable.ic_menu_directions, "Show Map", singleIntent);
-
-                notificationManager.notify(852, mBuilder.build());
-
-        }
-
     }
 
     public void showNotification(Intent intent) {
@@ -105,7 +62,7 @@ public class ActionNotificationBuilder {
         notificationManager.notify(852, mBuilder.build());
     }
 
-    public void showExpandableNotification(Intent intent) {
+    public void showNotificationWithActions(Intent intent, ArrayList<Intent> actions) {
         PendingIntent expandableIntent = PendingIntent.getActivity(
                 context,
                 0,
@@ -117,7 +74,6 @@ public class ActionNotificationBuilder {
                 .setSmallIcon(getNotificationSmallIconResource())
                 .setContentTitle(getTitle())
                 .setContentText(getSubTitle())
-                .setStyle(new NotificationCompat.BigTextStyle().bigText("Data"))
                 .setContentIntent(expandableIntent)
                 .setAutoCancel(true);
 
@@ -128,11 +84,22 @@ public class ActionNotificationBuilder {
             mBuilder.setSmallIcon(getNotificationSmallIconResource());
         }
 
+        for (Intent action : actions) {
+            PendingIntent buttonAction = PendingIntent.getActivity(
+                    context,
+                    0,
+                    action,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+            int iconResource = action.getIntExtra(BUTTON_ICON, android.R.drawable.ic_dialog_info);
+            mBuilder.addAction(iconResource, action.getStringExtra(BUTTON_ACTION), buttonAction);
+        }
+
         // Obtain NotificationManager system service in order to show the notification
         notificationManager.notify(852, mBuilder.build());
     }
 
-    public void showExpandableBigImageNotification(Intent intent) {
+    public void showBigImageNotification(Intent intent) {
         PendingIntent expandableIntent = PendingIntent.getActivity(
                 context,
                 0,
@@ -145,7 +112,6 @@ public class ActionNotificationBuilder {
                 .setLargeIcon(getNotificationBigIconResounce())
                 .setContentTitle(getTitle())
                 .setContentText(getSubTitle())
-                .setStyle(new NotificationCompat.BigTextStyle().bigText("Data"))
                 .setContentIntent(expandableIntent)
                 .setAutoCancel(true);
 
@@ -161,7 +127,7 @@ public class ActionNotificationBuilder {
     }
 
 
-    public void showExpandableBigImageNotificationWithActions(Intent intent, ArrayList<Intent> actions) {
+    public void showBigImageNotificationWithActions(Intent intent, ArrayList<Intent> actions) {
         PendingIntent expandableIntent = PendingIntent.getActivity(
                 context,
                 0,
@@ -188,7 +154,7 @@ public class ActionNotificationBuilder {
             PendingIntent buttonAction = PendingIntent.getActivity(
                     context,
                     0,
-                    intent,
+                    action,
                     PendingIntent.FLAG_UPDATE_CURRENT
             );
             int iconResource = action.getIntExtra(BUTTON_ICON, android.R.drawable.ic_dialog_info);
@@ -199,42 +165,107 @@ public class ActionNotificationBuilder {
         notificationManager.notify(852, mBuilder.build());
     }
 
-    public void showExpandableNotificationWithActions(Intent intent, ArrayList<Intent> actions) {
+
+    private void showExpandableNotification(Intent intent) {
         PendingIntent expandableIntent = PendingIntent.getActivity(
                 context,
-                0,
+                1,
                 intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-        );
+                PendingIntent.FLAG_UPDATE_CURRENT);
 
         mBuilder
                 .setSmallIcon(getNotificationSmallIconResource())
+                .setLargeIcon(getNotificationBigIconResounce())
                 .setContentTitle(getTitle())
                 .setContentText(getSubTitle())
                 .setContentIntent(expandableIntent)
                 .setAutoCancel(true);
 
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            mBuilder.setSmallIcon(getNotificationSmallIconResource());
-            mBuilder.setColor(context.getResources().getColor(android.R.color.white));
-        } else {
-            mBuilder.setSmallIcon(getNotificationSmallIconResource());
+        try {
+            Bitmap myBitmap = Glide.with(context)
+                    .load(getBackGroundImageUrl())
+                    .asBitmap()
+                    .fitCenter()
+                    .into(500, 500)
+                    .get();
+            NotificationCompat.BigPictureStyle s = new NotificationCompat.BigPictureStyle().bigPicture(myBitmap);
+            String title = getTitle();
+            String message = getSubTitle();
+            if (title.length() > 20) {
+                title = title.substring(0, 20);
+                title = title + "...";
+            }
+
+            if (message.length() > 100) {
+                message = message.substring(0, 100);
+                message = message + "...";
+            }
+            s.setBigContentTitle(title);
+            s.setSummaryText(message);
+            mBuilder.setStyle(s);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        notificationManager.notify(852, mBuilder.build());
+    }
+
+    private void showExpandableNotificationWithAction(Intent intent, ArrayList<Intent> actions) {
+        PendingIntent expandableIntent = PendingIntent.getActivity(
+                context,
+                1,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder
+                .setSmallIcon(getNotificationSmallIconResource())
+                .setLargeIcon(getNotificationBigIconResounce())
+                .setContentTitle(getTitle())
+                .setContentText(getSubTitle())
+                .setContentIntent(expandableIntent)
+                .setAutoCancel(true);
+
+        try {
+            Bitmap myBitmap = Glide.with(context)
+                    .load(getBackGroundImageUrl())
+                    .asBitmap()
+                    .fitCenter()
+                    .into(500, 500)
+                    .get();
+            NotificationCompat.BigPictureStyle s = new NotificationCompat.BigPictureStyle().bigPicture(myBitmap);
+            String title = getTitle();
+            String message = getSubTitle();
+            if (title.length() > 20) {
+                title = title.substring(0, 20);
+                title = title + "...";
+            }
+
+            if (message.length() > 100) {
+                message = message.substring(0, 100);
+                message = message + "...";
+            }
+            s.setBigContentTitle(title);
+            s.setSummaryText(message);
+            mBuilder.setStyle(s);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
 
         for (Intent action : actions) {
             PendingIntent buttonAction = PendingIntent.getActivity(
                     context,
                     0,
-                    intent,
+                    action,
                     PendingIntent.FLAG_UPDATE_CURRENT
             );
             int iconResource = action.getIntExtra(BUTTON_ICON, android.R.drawable.ic_dialog_info);
             mBuilder.addAction(iconResource, action.getStringExtra(BUTTON_ACTION), buttonAction);
         }
 
-        // Obtain NotificationManager system service in order to show the notification
+
         notificationManager.notify(852, mBuilder.build());
     }
+
 
     public void cancelNotification() {
         notificationManager.cancel(852);
@@ -270,5 +301,13 @@ public class ActionNotificationBuilder {
 
     public void setSubTitle(String subTitle) {
         this.subTitle = subTitle;
+    }
+
+    public String getBackGroundImageUrl() {
+        return backGroundImageUrl;
+    }
+
+    public void setBackGroundImageUrl(String backGroundImageUrl) {
+        this.backGroundImageUrl = backGroundImageUrl;
     }
 }
